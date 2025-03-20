ce509bcd5c9cc5f91dda1471e45107605c0988ec
robert-szg <155324734+robert-szg@users.noreply.github.com>
2025-03-20 10:44:19 -0300

    Initial Commit

---
+++ git_remote_repo.py
@@ -0,0 +1,22 @@
+print("This is a small test")
+
+# Create a Remote Repo
+print("git init -b main")
+print("git config --list")
+print("git commit -m \"Initial Commit\"")
+print("git remote add origin URL")
+print("git remote -v")
+print("git add .")
+print("git push -u origin main")
+
+# Change user and e-mail 
+print("git config --global user.name \"name\"")
+print("git config --global user.email \"email might be private, better check in GitHub\"")
+print("git config user.name \"name\"")
+print("git config user.email \"email might be private, better check in GitHub\"")
+
+# Change branch name from master to main
+print("git branch -m master main")
+print("git branch")
+
+
